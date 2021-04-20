384dad251042587272c58da1f32af02b5e7aef14
Alex Gunasekera <alexander.gunasekera@gmail.com>
2021-04-20 14:45:35 +0100

    Normal ordering

--- COSCC/Wick.py
+++ COSCC/Wick.py
@@ -1,50 +1,50 @@
+import numpy as np
 from pyscf import fci
+from copy import deepcopy, copy
 
 #define vacuum
 #take in operator string
 #format: string of indices, bitstring of creation vs annihilation
 #return normal-ordered operator string plus contracted terms, according to Wick's theorem
 
-class operatorString:
-    def __init__(self, indexString, creationOrAnnihilation, alphaOrBeta):
-        #list of indices in the string of second-quantised operators
-        self.iS = indexString
-        #bitstring of same length, with 1 corresponding to creation and 0 for annihilation
-        self.cOA = creationOrAnnihilation
-        #bitstring of same length, with 1 corresponding to alpha and 0 for beta
-        self.aOB = alphaOrBeta
+#Class for a single second-quantized operator
+class basicOperator:
+    def __init__(self, orbital_, creation_annihilation_, spin_):
+        self.orbital = orbital_ #orbital index
+        self.spin = spin_ #1 for alpha, 0 for beta
+        self.creation_annihilation = creation_annihilation_ #1 for creation, 0 for annihilation
+#        self.quasi_cre_ann = None
 
-    #Apply the string of second-quantised operators to an arbitrary state
-    def applyOperator(self, state, norb, (nelec, nelecb)):
-        i = len(self.iS)
-        while(i > 0):
-            i = i - 1
-            print(i)
-            print(self.iS[i])
-            print(self.cOA[i])
-            print(self.aOB[i])
-            if self.cOA[i]:
-                if self.aOB[i]:
-                    state = fci.addons.cre_a(state, norb, (neleca, nelecb)), self.iS[i])
-                    neleca = neleca + 1
-                else:
-                    state = fci.addons.cre_b(state, norb, (neleca, nelecb), self.iS[i])
-                    nelecb = nelecb + 1
+    def apply(self, state):
+        neleca, nelecb = state.nelec[0], state.nelec[1]
+        norb = state.norb
+        if bool(self.creation_annihilation):
+            if bool(self.spin):
+#                if neleca == norb:
+#                    state.array = np.array([[0.]])
+#                    state.nelec = (0, 0)
+#                else:
+                state.array = fci.addons.cre_a(state.array, state.norb, state.nelec, self.orbital)
+                state.nelec = (neleca + 1, nelecb)
             else:
-                if self.aOB[i]:
-                    state = fci.addons.des_a(state, norb, (neleca, nelecb), self.iS[i])
-                    neleca = neleca - 1
-                else:
-                    state = fci.addons.des_b(state, norb, (neleca, nelecb), self.iS[i])
-                    nelecb = nelecb - 1
-#        return state
-
-    def printstuff(self):
-        i = len(self.iS)
-        while(i > 0):
-            i = i - 1
-            print(i)
-            print(self.iS[i])
-            print(self.cOA[i])
-            print(self.aOB[i])
-        return 0
+#                if nelecb == norb:
+#                    state.array = np.array([[0.]])
+#                    state.nelec = (0, 0)
+#                else:
+                state.array = fci.addons.cre_b(state.array, state.norb, state.nelec, self.orbital)
+                state.nelec = (neleca, nelecb + 1)
+        else:
+            if bool(self.spin):
+#                if neleca == 0:
+#                    state.array = np.array([[0.]])
+#                    state.nelec = (0, 0)
+#                else:
+                state.array = fci.addons.des_a(state.array, state.norb, state.nelec, self.orbital)
+                state.nelec = (neleca - 1, nelecb)
+            else:
+#                if nelecb == 0:
+#                    state.array = np.array([[0.]])
+#                    state.nelec = (0, 0)
+#                else:
+                state.array = fci.addons.des_b(state.array, state.norb, state.nelec, self.orbital)
+                state.nelec = (neleca, nelecb - 1)
